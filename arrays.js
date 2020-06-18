a733b22d056037afb9e96cb92cc4c54c72a0f105
Samara Grant <samara.amanda01@gmail.com>
2020-06-18 20:20:27 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -19,5 +19,5 @@ function destructivelyAddElementToEndOfArray(array, element) {
 }
 
 function accessElementInArray(array, index) {
-  console.log(array[0] + element)
+  return array[index] + element
 }
