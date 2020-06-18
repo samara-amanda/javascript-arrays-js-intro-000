840e16dfc8b8185e9db1afc4e79cb42445d4aff0
Samara Grant <samara.amanda01@gmail.com>
2020-06-18 19:38:16 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,10 +10,10 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray (array, element) {
-  array.push(element)
-  return array
+  return [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  return [...array, element]
+  array.push(element)
+  return array
 }
