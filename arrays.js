7214d1c0805b16dcd7cb3fdefad255f4405c97c2
Samara Grant <samara.amanda01@gmail.com>
2020-06-19 02:44:29 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -38,5 +38,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  return array.slice(2, array.length - 1)
+  array.slice(2, array.length - 1)
+  return array
 }
